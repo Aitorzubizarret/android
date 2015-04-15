8a064debe6f13ffe7863f884d2164adbedd2dcba
aitorzubizarret <aitorzubizarreta@yahoo.es>
2015-04-15 18:32:11 +0200

    Cambios

--- Terremotos/app/src/main/java/com/aitorzubizarreta/terremotos/Adapters/TerremotoAdapter.java
+++ Terremotos/app/src/main/java/com/aitorzubizarreta/terremotos/Adapters/TerremotoAdapter.java
@@ -6,8 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.aitorzubizarreta.terremotos.Model.Terremoto;
+import com.aitorzubizarreta.terremotos.R;
 
 import java.util.List;
 
@@ -18,6 +20,8 @@ public class TerremotoAdapter extends ArrayAdapter<Terremoto> {
 
     // Var
     private int resource;
+    private Terremoto terremoto;
+    private TextView terremotoName;
 
     public TerremotoAdapter(Context context, int resource, List<Terremoto> objects) {
         super(context, resource, objects);
@@ -39,6 +43,15 @@ public class TerremotoAdapter extends ArrayAdapter<Terremoto> {
         } else {
             layout = (LinearLayout)convertView;
         }
+
+        // Obtenemos el terremoto para después obtener sus datos
+        terremoto = getItem(position);
+
+        // Obtenemos los elementos del layout (item_terremoto.xml) para poder imprimir los datos en ellos
+        terremotoName = (TextView)layout.findViewById(R.id.textViewTerremotoName);
+
+        // Obtenemos los datos del terremoto y se los asignamos a los campos del layout para mostrarlos en pantalla
+        terremotoName.setText(terremoto.getName());
 
         return layout;
     }
--- Terremotos/app/src/main/java/com/aitorzubizarreta/terremotos/Model/Terremoto.java
+++ Terremotos/app/src/main/java/com/aitorzubizarreta/terremotos/Model/Terremoto.java
@@ -4,4 +4,13 @@ package com.aitorzubizarreta.terremotos.Model;
  * Created by cursomovil on 14/04/15.
  */
 public class Terremoto {
+    private String name;
+
+    public Terremoto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
